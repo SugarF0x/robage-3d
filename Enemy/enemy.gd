0bfc9b4ea765aae85c4cd2eed7c44b92e5bd946c
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-16 02:07:58 +0300

    have enemy follow player

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -20,20 +20,11 @@ func _process(delta: float) -> void:
 	navigation_agent_3d.target_position = player.global_position
 
 func _physics_process(delta: float) -> void:
-	var next_position = navigation_agent_3d.get_next_path_position()
+	if not is_on_floor(): velocity.y -= gravity * delta
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var next_position = navigation_agent_3d.get_next_path_position()
+	var direction := global_position.direction_to(next_position)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -41,4 +32,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	#move_and_slide()
+	move_and_slide()
