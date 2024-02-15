f31dd3455ab404f67a80429581a25f48d512eac9
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-16 02:13:16 +0300

    add enemy aggro range

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 
 
+@export var aggro_range := 12.0
+
+
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 
 
@@ -10,6 +13,7 @@ const JUMP_VELOCITY = 4.5
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var player: CharacterBody3D
+var provoked := false
 
 
 func _ready() -> void:
@@ -21,6 +25,9 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor(): velocity.y -= gravity * delta
+	
+	provoked = provoked or global_position.distance_to(player.global_position) <= aggro_range
+	if not provoked: return
 	
 	var next_position = navigation_agent_3d.get_next_path_position()
 	var direction := global_position.direction_to(next_position)
